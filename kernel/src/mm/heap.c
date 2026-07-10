31b62171b66cb2d0a31d6deeae5ca6a74eb4d5b8
mihirmalaviya <mihir.malaviya@rutgers.edu>
2026-07-10 00:52:23 -0400

    added panic

--- kernel/src/mm/heap.c
+++ kernel/src/mm/heap.c
@@ -1,6 +1,7 @@
 #include "mm/heap.h"
 #include "mm/vmm.h"
 #include "mm/pmm.h"
+#include "kernel.h"
 
 #define PAGE_SIZE  4096ULL
 #define HEAP_START 0x444444440000ULL
@@ -17,14 +18,15 @@ void *kmalloc(size_t size) {
     size = (size + 15) & ~15ULL; // round up to 16 bytes
 
 		if (heap_cursor+size > HEAP_START+HEAP_SIZE) {
+				panic("kmalloc: heap exhausted");
 				return NULL;
 		}
 
 		uint64_t end = heap_cursor + size;
 		while (heap_mapped_until < end) {
-			uint64_t phys = pmm_alloc();
-			vmm_map(vmm_get_current_pml4(), heap_mapped_until, phys, VMM_PRESENT | VMM_WRITABLE);
-			heap_mapped_until += PAGE_SIZE;
+				uint64_t phys = pmm_alloc();
+				vmm_map(vmm_get_current_pml4(), heap_mapped_until, phys, VMM_PRESENT | VMM_WRITABLE);
+				heap_mapped_until += PAGE_SIZE;
 		}
 
 		void *ptr = (void *)heap_cursor;
